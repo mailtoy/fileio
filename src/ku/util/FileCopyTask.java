2e886d8a20144cf0d499cd8b81ca9b1b46e35126
Kanchanok Kannee <kanchanok.kan@ku.th>
2017-03-05 14:52:45 +0700

    Finished code

--- src/ku/util/FileCopyTask.java
+++ src/ku/util/FileCopyTask.java
@@ -9,25 +9,24 @@ import java.io.OutputStream;
 import stopwatch.TaskTimer;
 
 /**
- * This class defines code used to create Runnable 'tasks' to test
- * the file copy mehods in FileUtil.  The subclasses should
- * define their own run() method to perform the actual task.
+ * This class defines code used to create Runnable 'tasks' to test the file copy
+ * mehods in FileUtil. The subclasses should define their own run() method to
+ * perform the actual task.
  * 
- * See the main method for example of how to use this.
- * It uses the Stopwatch and TaskTimer classes from stopwatch project.
+ * See the main method for example of how to use this. It uses the Stopwatch and
+ * TaskTimer classes from stopwatch project.
  * 
- * All the file copy methods require an input stream (read from file)
- * and an output stream that writes to a file, so this class defines
- * methods to open a file as an InputStream and open an output file 
- * as an OutputStream.  Files can be opened via the constructor or
- * setInput(filename) and setOutput(filename) methods.
+ * All the file copy methods require an input stream (read from file) and an
+ * output stream that writes to a file, so this class defines methods to open a
+ * file as an InputStream and open an output file as an OutputStream. Files can
+ * be opened via the constructor or setInput(filename) and setOutput(filename)
+ * methods.
  * 
- * The method to open an InputStream shows how to use the ClassLoader
- * to find a file on the classpath of this project.  The classpath
- * includes files in your project's src/ directory.  
- * It is a standard technique for opening resources.
+ * The method to open an InputStream shows how to use the ClassLoader to find a
+ * file on the classpath of this project. The classpath includes files in your
+ * project's src/ directory. It is a standard technique for opening resources.
  * 
- * @author 
+ * @author  Kanchanok Kannee
  *
  */
 public class FileCopyTask implements Runnable {
@@ -35,15 +34,18 @@ public class FileCopyTask implements Runnable {
 	protected InputStream in = null;
 	/** The OutputStream that data will be written to. */
 	protected OutputStream out = null;
-	
-	/** Default constructor doesn't do anything but may be
-	 *  needed for subclasses that don't invoke parameterized constructor. 
-	 */
-	public FileCopyTask() { }
-	
+
 	/**
-	 * Initialize a FileCopyTask with names of the input and output
-	 * files to use.
+	 * Default constructor doesn't do anything but may be needed for subclasses
+	 * that don't invoke parameterized constructor.
+	 */
+	public FileCopyTask() {
+	}
+
+	/**
+	 * Initialize a FileCopyTask with names of the input and output files to
+	 * use.
+	 * 
 	 * @param infile name of the file to use as input
 	 * @param outfile name of the file to use as output
 	 * @throws RuntimeException if either file cannot be opened
@@ -52,16 +54,17 @@ public class FileCopyTask implements Runnable {
 		setInput(infile);
 		setOutput(outfile);
 	}
-	
+
 	/**
 	 * Set the file to use as this object's 'in' attribute (InputStream).
+	 * 
 	 * @param filename is the name of a file to read as input
-	 * @throws RuntimeException if the filename cannot be opened for
-	 *    input, which usually means file not found.
+	 * @throws RuntimeExceptio if the filename cannot be opened for input, which usually
+	 *             means file not found.
 	 */
 	public void setInput(String filename) {
-		 in = null;
-		 filename = "src/Big-Alice-in-Wonderland.txt";
+		in = null;
+		filename = "src/Big-Alice-in-Wonderland.txt";
 		try {
 			// If the filename is an absolute path or is in the "current"
 			// directory then using FileInputStream should open it.
@@ -69,133 +72,127 @@ public class FileCopyTask implements Runnable {
 		} catch (FileNotFoundException fne) {
 			// ignore it and try again
 		}
-		if (in != null) return;
+		if (in != null)
+			return;
 		// The ClassLoader knows the application's classpath
 		// and can open files that are on the classpath.
 		// The filename can have a relative directory to refer to
 		// subdirectories of the project source tree.
 		ClassLoader loader = this.getClass().getClassLoader();
 		in = loader.getResourceAsStream(filename);
-		
+
 		// If loader.getResourceAsStream() cannot create an InputStream
-		// then it returns null.  (No exception is thrown.)
-		// If 'in' is null then throw a RuntimeException 
+		// then it returns null. (No exception is thrown.)
+		// If 'in' is null then throw a RuntimeException
 		// so the caller will know that filename could not be opened.
-		
-		//TODO If in (InputStream) is null, throw a RuntimeException with a message.
-	if(in == null) {
-		throw new RuntimeException("Find could not be opened.");
+
+		if (in == null) {
+			throw new RuntimeException("Find could not be opened." + filename);
+		}
 	}
-	}
-	
-	
+
 	/**
 	 * Specify a filename to use as the OutputStream (out attribute).
 	 * 
-	 * @param filename is the name of the file to write to.
-	 *     If the file already exists it will be overwritten.
-	 * @throws RuntimeException if the filename cannot be opened as
-	 *     an OutputStream.
+	 * @param filename is the name of the file to write to. If the file already exists it will be overwritten.
+	 * @throws RuntimeExceptio if the filename cannot be opened as an OutputStream.
 	 */
 	public void setOutput(String filename) {
 		try {
-			// This is easy.  Use FileOutputStream.
+			// This is easy. Use FileOutputStream.
 			out = new FileOutputStream(filename);
-		} catch (FileNotFoundException fne ) {
+		} catch (FileNotFoundException fne) {
 			// rethrow it as an unchecked exception
-			throw new RuntimeException("could not open output file "+filename, fne);
+			throw new RuntimeException("could not open output file " + filename, fne);
 		}
 	}
-	
+
 	/**
-	 * The run() method should be overridden by subclasses
-	 * to perform a task.
+	 * The run() method should be overridden by subclasses to perform a task.
 	 */
 	public void run() {
 		System.out.println("You forgot to override run in subclass.");
 	}
-	
+
 	/**
-	 * The toString() method should be overridden by subclasses
-	 * to describe the task.
+	 * The toString() method should be overridden by subclasses to describe the
+	 * task.
 	 */
 	public String toString() {
 		return "Pay attention! You forgot to write toString in subclass.";
 	}
 
 	/**
-	 * This main method could be in a separate class, for clarity.
-	 * It uses this class to create subclasses for each task.
-	 * It uses Stopwatch and TaskTimer to execute the task.
-	 * @param args
+	 * This main method could be in a separate class, for clarity. It uses this
+	 * class to create subclasses for each task. It uses Stopwatch and TaskTimer
+	 * to execute the task.
+	 * 
+	 * @param args not used.
 	 */
 	public static void main(String[] args) {
 		final String inputFilename = "Big-Alice-in-Wonderland.txt";
 		final String outputFilename = "filecopy1.txt";
-		
+
 		// Define a FileUtil task to copy a file byte by byte.
 		// This is an anonymous class that extends FileUtilTimer.
 		FileCopyTask task1 = new FileCopyTask(inputFilename, outputFilename) {
 			public void run() {
 				FileUtil.copy(in, out);
 			}
+
 			public String toString() {
 				return "Copy a file byte-by-byte";
 			}
 		};
-		
+
 		int oneKB = 1024;
 		FileCopyTask task2 = new FileCopyTask(inputFilename, outputFilename) {
 			public void run() {
-				FileUtil.copy(in, out,oneKB);
+				FileUtil.copy(in, out, oneKB);
 			}
+
 			public String toString() {
 				return "Copy a file byte array-1KB";
 			}
 		};
-		
+
 		int fourKB = 4 * oneKB;
 		FileCopyTask task3 = new FileCopyTask(inputFilename, outputFilename) {
 			public void run() {
-				FileUtil.copy(in, out,fourKB);
+				FileUtil.copy(in, out, fourKB);
 			}
+
 			public String toString() {
 				return "Copy a file byte array-4KB";
 			}
 		};
-		
-		int sixty_fourKB = 64 *oneKB;
+
+		int sixty_fourKB = 64 * oneKB;
 		FileCopyTask task4 = new FileCopyTask(inputFilename, outputFilename) {
 			public void run() {
-				FileUtil.copy(in, out,sixty_fourKB);
+				FileUtil.copy(in, out, sixty_fourKB);
 			}
+
 			public String toString() {
 				return "Copy a file byte array-64KB";
 			}
 		};
-		
+
 		FileCopyTask task5 = new FileCopyTask(inputFilename, outputFilename) {
 			public void run() {
 				FileUtil.bcopy(in, out);
 			}
+
 			public String toString() {
 				return "Copy a file line by line";
 			}
 		};
-		
+
 		TaskTimer timer = new TaskTimer();
-		timer.measureAndPrint(task1);  
+		timer.measureAndPrint(task1);
 		timer.measureAndPrint(task2);
 		timer.measureAndPrint(task3);
 		timer.measureAndPrint(task4);
 		timer.measureAndPrint(task5);
-		
-//		BufferedReaderTask mBufferedReaderTask = new BufferedReaderTask(inputFilename, outputFilename);
-//		BlockCopyTask b = new BlockCopyTask(inputFilename, outputFilename,fourKB);
-//		BlockCopyTask blockCopyTask = new BlockCopyTask(inputFilename, outputFilename,sixty_fourKB);
-//		timer.measureAndPrint(mBufferedReaderTask);
-//		timer.measureAndPrint(b);
-//		timer.measureAndPrint(blockCopyTask);
-		
 	}
 }
--- src/ku/util/FileUtil.java
+++ src/ku/util/FileUtil.java
@@ -7,63 +7,92 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 
+/**
+ * This class is use to read a file and copy by InputSteam, OutputSteam,
+ * BufferedReader, and PrintWriter.
+ * 
+ * @author Kanchanok Kannee
+ *
+ */
 public class FileUtil {
-	
-	 static void copy(InputStream in, OutputStream out) {
-		 int word = 0;
-		 try {
-			 while((word = in.read()) != -1) {
-				 out.write(word);
-			 }
-			 in.close();
-			 out.close();
-		 } catch (Exception e) {
-			 throw new RuntimeException();
+
+	/**
+	 * Copy a file of InputStream to OutputStream byte-by-byte.
+	 * 
+	 * @param in is the Object of InputSteam to be read
+	 * @param out is the Object of OutputSteam to be write.
+	 */
+	static void copy(InputStream in, OutputStream out) {
+		int word = 0;
+		try {
+			while ((word = in.read()) != -1) {
+				out.write(word);
+				}
+			} catch (Exception e) {
+				throw new RuntimeException();
+				} finally {
+					try {
+						in.close();
+						out.close();
+						} catch (IOException e) {
+							
+						}
+					}
 		}
-		 
-		 
-		
-	}
-	
-	 static void copy(InputStream in, OutputStream out, int blocksize)  {
-	
+
+	/**
+	 * Copy a file of InputSteam to the OutputSteam using byte array of size
+	 * blocksize.
+	 * 
+	 * @param in is the Object of InputStream to be read.
+	 * @param out is the Object of OutputSteam to be write.
+	 * @param blocksize is amount of byte to copy at a time.
+	 */
+	static void copy(InputStream in, OutputStream out, int blocksize) {
+
 		byte[] buffer = new byte[blocksize];
 		try {
 			while (in.read(buffer) > 0) {
 				out.write(buffer);
-			}
-			in.close();
-			out.close();
-		} catch (IOException e) {
-			throw new RuntimeException();
-		
-		
+				}
+			} catch (IOException e) {
+				throw new RuntimeException();
+				} finally {
+					try {
+						in.close();
+						out.close();
+						} catch (IOException e) {
+							
+						}
+					}
 		}
-		
-		
-	}
-	
-	static void bcopy(InputStream in, OutputStream out) {
-//		ช่วยให้มันเป็นstring
-		PrintWriter printWriter = new PrintWriter(out);
-//		อ่านข้อมูที่มีเป็นInputStreamReader แล้วข้อมูลอยูในin
-//	เปลี่ยนinputstream ให้เป็น reader
 
-		//		
+	/**
+	 * Copy a file of InputSteam to the OutputSteam using BufferedReader to read the
+	 * InputSteam and PrintWriter to write the OutputSteam and read line by line at
+	 * a time.
+	 * 
+	 * @param in is the Object of InputStream.
+	 * @param out is the Object of OutputSteam to be write.
+	 */
+	static void bcopy(InputStream in, OutputStream out) {
+
+		PrintWriter printWriter = new PrintWriter(out);
 		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-		String text ;
+		String text;
 		try {
 			while ((text = reader.readLine()) != null) {
 				printWriter.write(text);
-			}
-			reader.close();
-			printWriter.close();
-			in.close();
-			out.close();
-		} catch (IOException e) {
-			throw new RuntimeException();
-		}	
-	}
-	
+				}
+			} catch (IOException e) {
+				throw new RuntimeException();
+				} finally {
+					try {
+						in.close();
+						out.close();
+						} catch (IOException e) {
 
+						}
+					}
+		}
 }
